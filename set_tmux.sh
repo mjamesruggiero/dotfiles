6b36eb0d89682cb4e1efa6d9ca7eaf5bc4504a72
mjamesruggiero <mjamesruggiero@gmail.com>
2014-07-25 23:20:51 -0700

    new workflow for tmux

--- set_tmux.sh
+++ set_tmux.sh
@@ -19,76 +19,49 @@ git_window() {
 georgina() {
     dir="${BASE_DIR}/code/mr/georgina"
     build_session "georgina"
-
-    # repl
-    #tmux rename-window -t 0 -n repl
     tmux send-keys "cd $dir" C-m
     tmux send-keys "./sbt" C-m
-
-    # vim
     vim_window $dir
-
-    # git
     git_window $dir
 }
 
-lily() {
-    dir="${BASE_DIR}/code/mr/lily"
-    build_session "lily"
-
-    # repl
-    #tmux rename-window -t 0 -n repl
+waldorf() {
+    dir="${BASE_DIR}/code/mr/waldorf"
+    build_session "waldorf"
     tmux send-keys "cd $dir" C-m
-    tmux send-keys "workon ipy" C-m
-
-    # vim
     vim_window $dir
-
-    # git
     git_window $dir
 }
 
-sharethrough() {
+bakery() {
     dir="${BASE_DIR}/code/workspace/bakery"
-    build_session sharethrough
-
-    # repl
-    #tmux rename-window -t 0 -n repl
+    build_session "bakery"
     tmux send-keys "cd $dir" C-m
-
-    # vim
     vim_window $dir
-
-    # git
     git_window $dir
 }
 
 hq() {
     dir="${BASE_DIR}/code/workspace/Creatorious"
     build_session "HQ"
-
     tmux send-keys "cd $dir" C-m
-
-    # vim
     vim_window $dir
 }
 
-katas() {
-    dir="${BASE_DIR}/code/mr/katas"
-    build_session "katas"
-
+dickens() {
+    dir="${BASE_DIR}/code/mr/dickens"
+    build_session "dickens"
     tmux send-keys "cd $dir" C-m
-
-    # vim
+    git_window $dir
     vim_window $dir
 }
 
 #build them
-sharethrough
+bakery
 hq
 georgina
-#lily
-katas
+waldorf
+dickens
 
 #and attach me
 tmux attach-session -t georgina
