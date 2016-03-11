a4e735b6c3f4a845c4ffd950424e3abcf39da38b
mjamesruggiero <mjamesruggiero@gmail.com>
2016-03-10 21:00:05 -0800

    Vim sexp mappings

--- import_pathogen.rb
+++ import_pathogen.rb
@@ -1,28 +1,30 @@
 #!/usr/bin/env ruby
 
 git_bundles = %w{
+  git://git.wincent.com/command-t.git
+  git://github.com/alfredodeza/pytest.vim.git
+  git://github.com/derekwyatt/vim-scala.git
+  git://github.com/guns/vim-clojure-static.git
+  git://github.com/guns/vim-sexp.git
+  git://github.com/kchmck/vim-coffee-script.git
+  git://github.com/klen/python-mode.git
+  git://github.com/msanders/snipmate.vim.git
+  git://github.com/nvie/vim-flake8.git
+  git://github.com/rizzatti/dash.vim.git
+  git://github.com/rking/ag.vim
+  git://github.com/scrooloose/nerdcommenter.git
   git://github.com/scrooloose/nerdtree.git
+  git://github.com/thoughtbot/vim-rspec.git
+  git://github.com/tpope/vim-classpath.git
+  git://github.com/tpope/vim-fireplace.git
   git://github.com/tpope/vim-fugitive.git
   git://github.com/tpope/vim-haml.git
   git://github.com/tpope/vim-rails.git
   git://github.com/tpope/vim-repeat.git
+  git://github.com/tpope/vim-sexp-mappings-for-regular-people.git
   git://github.com/tpope/vim-surround.git
   git://github.com/tsaleh/vim-matchit.git
-  git://github.com/msanders/snipmate.vim.git
-  git://github.com/nvie/vim-flake8.git
-  git://github.com/alfredodeza/pytest.vim.git
-  git://github.com/derekwyatt/vim-scala.git
-  git://github.com/tpope/vim-classpath.git
-  git://github.com/guns/vim-clojure-static.git
-  git://github.com/rking/ag.vim
-  git://github.com/scrooloose/nerdcommenter.git
-  git://github.com/kchmck/vim-coffee-script.git
-  git://github.com/tpope/vim-fireplace.git
-  git://github.com/thoughtbot/vim-rspec.git
   git://github.com/walm/jshint.vim.git
-  git://git.wincent.com/command-t.git
-  git://github.com/klen/python-mode.git
-  git://github.com/rizzatti/dash.vim.git
 }
 
 require 'fileutils'
