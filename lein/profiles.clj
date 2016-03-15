26f8ba1d81b74947931b59878538b1637317bb90
mjamesruggiero <mjamesruggiero@gmail.com>
2016-03-14 21:21:49 -0700

    Add Clojure linters to lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,1 +1,6 @@
-{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.10.0"]]}}
+{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.10.0"]
+                  [venantius/ultra  "0.4.1"]
+                  [lein-kibit  "0.1.2"]
+                  [jonase/eastwood  "0.2.3"]
+                  [lein-bikeshed  "0.3.0"]
+                  [lein-ancient "0.6.8"]]}}
