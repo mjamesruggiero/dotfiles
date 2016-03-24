34224914efeb261957873b9a22910f839f12056a
mjamesruggiero <mjamesruggiero@gmail.com>
2016-03-24 16:06:16 -0700

    Remove ultra from lein profile
    * doesn't play well with vim-fireplace

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,4 @@
 {:user {:plugins [[com.jakemccrary/lein-test-refresh "0.10.0"]
-                  [venantius/ultra  "0.4.1"]
                   [lein-kibit  "0.1.2"]
                   [jonase/eastwood  "0.2.3"]
                   [lein-bikeshed  "0.3.0"]
