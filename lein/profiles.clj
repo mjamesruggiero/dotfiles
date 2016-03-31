628440022f446b9e478aeb14f6d97831ffce2c1a
mjamesruggiero <mjamesruggiero@gmail.com>
2016-03-30 21:47:38 -0700

    Leinigen provides code coverage

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,5 @@
                   [lein-kibit  "0.1.2"]
                   [jonase/eastwood  "0.2.3"]
                   [lein-bikeshed  "0.3.0"]
-                  [lein-ancient "0.6.8"]]}}
+                  [lein-ancient "0.6.8"]
+                  [lein-cloverage "1.0.6"]]}}
